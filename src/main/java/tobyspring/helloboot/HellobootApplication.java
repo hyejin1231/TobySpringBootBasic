3a65226b52c69626709baf76e74d1160bd145c06
hyejin1231 <phj981231@naver.com>
2023-06-19 08:22:19 +0900

    20230619 토비의 스프링 부트 이해와 원리 #2. 독립 실행형 서블릿 애플리케이션 서블릿 컨테이너 띄워보기.

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,15 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		// 서블릿 컨테이너 띄워보기. 
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 
 }
