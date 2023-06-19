b7a8d042a9a46a25a1d07ef228b989853e7e98b4
hyejin1231 <phj981231@naver.com>
2023-06-20 08:29:48 +0900

    20230619 토비의 스프링 부트 이해와 원리 #2. 독립 실행형 스프링 애플리케이션 의존 오브젝트 SimpleHelloService 추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,9 +1,16 @@
 package tobyspring.helloboot;
 
+import java.util.Objects;
+
+/**
+ * 컨트롤러의 중요한 역할 중 하나는 넘어온 파라미터 값을 검증하는 것 !
+ */
 public class HelloController {
 
     public String hello(String name) {
-        return "Hello " + name + "!";
+        SimpleHelloService helloService = new SimpleHelloService();
+        
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 
 }
