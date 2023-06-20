3bb9747741f0f2a35128968d23ff1d53073fdedf
hyejin1231 <phj981231@naver.com>
2023-06-20 09:15:38 +0900

    20230620 토비의 스프링 부트 이해와 원리 #3. 독립 실행형 스프링 애플리케이션 애플리케이션 매핑 정보 사용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,9 +2,17 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 컨트롤러의 중요한 역할 중 하나는 넘어온 파라미터 값을 검증하는 것 !
  */
+@Controller
+@RequestMapping
 public class HelloController {
     
     // final을 같이 선언하는 이유 : 한번 할당하면 재할당을 할 수 없음
@@ -15,6 +23,8 @@ public class HelloController {
         this.helloService = helloService;
     }
     
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         
         return helloService.sayHello(Objects.requireNonNull(name));
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,21 +1,9 @@
 package tobyspring.helloboot;
 
-import java.io.IOException;
-
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 public class HellobootApplication {
 
