9ee5689fde0020515115391840c91095faa4cb11
hyejin1231 <phj981231@naver.com>
2023-06-20 09:51:56 +0900

    20230620 토비의 스프링 부트 이해와 원리 #3. 독립 실행형 스프링 애플리케이션 @Component 스캔

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,16 +2,13 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 컨트롤러의 중요한 역할 중 하나는 넘어온 파라미터 값을 검증하는 것 !
  */
-@Controller
+@RestController
 public class HelloController {
     
     // final을 같이 선언하는 이유 : 한번 할당하면 재할당을 할 수 없음
@@ -23,7 +20,6 @@ public class HelloController {
     }
     
     @GetMapping("/hello")
-    @ResponseBody
     public String hello(String name) {
         
         return helloService.sayHello(Objects.requireNonNull(name));
