c1b523144f38e5dcb85f4964c39b027bdf76c042
hyejin1231 <phj981231@naver.com>
2023-06-20 15:29:28 +0900

    20230620 토비의 스프링 부트 이해와 원리 #3. 독립 실행형 스프링 애플리케이션 SpringBootApplication

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,8 +1,10 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -11,31 +13,23 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
-	
-	public static void main(String[] args) {
-		
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
-			@Override
-			protected void onRefresh()
-			{
-				super.onRefresh();
-				
-				/**
-				 * 서블릿 컨테이너 띄우기
-				 * TomcatServletWebServerFactory : 스프링 부트가 제공하는 내장형 톰캣의 초기화 작업과 설정 지원 클래스
-				 */
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					
-					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)
-					).addMapping("/*");
-				});
-				webServer.start();
-			}
-		};
-		applicationContext.register(HellobootApplication.class);
-		applicationContext.refresh();
-		
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory()
+	{
+		return new TomcatServletWebServerFactory();
 	}
 
+	@Bean
+	public DispatcherServlet dispatcherServlet()
+	{
+		return new DispatcherServlet();
+	}
+
+	
+	public static void main(String[] args) {
+//		MySpringApplication.run(HellobootApplication.class, args);
+		SpringApplication.run(HellobootApplication.class, args);
+	}
+	
+	
 }
