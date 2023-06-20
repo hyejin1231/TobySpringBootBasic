e1078bdab141e3c37073649c6554920adc93bdeb
hyejin1231 <phj981231@naver.com>
2023-06-20 09:26:54 +0900

    20230620 토비의 스프링 부트 이해와 원리 #3. 독립 실행형 스프링 애플리케이션 스프링 컨테이너로 통합

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
  * 컨트롤러의 중요한 역할 중 하나는 넘어온 파라미터 값을 검증하는 것 !
  */
 @Controller
-@RequestMapping
 public class HelloController {
     
     // final을 같이 선언하는 이유 : 한번 할당하면 재할당을 할 수 없음
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,6 +2,7 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -9,23 +10,29 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+			@Override
+			protected void onRefresh()
+			{
+				super.onRefresh();
+				
+				/**
+				 * 서블릿 컨테이너 띄우기
+				 * TomcatServletWebServerFactory : 스프링 부트가 제공하는 내장형 톰캣의 초기화 작업과 설정 지원 클래스
+				 */
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					
+					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)
+					).addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
-
 		
-		/**
-		 * 서블릿 컨테이너 띄우기
-		 * TomcatServletWebServerFactory : 스프링 부트가 제공하는 내장형 톰캣의 초기화 작업과 설정 지원 클래스
-		 */
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			
-			servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext)
-			).addMapping("/*");
-		});
-		webServer.start();
 	}
 
 }
