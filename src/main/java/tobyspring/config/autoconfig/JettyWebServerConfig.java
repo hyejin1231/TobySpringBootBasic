f0d2a8c0be84988fd692c6b0e47a21e3daee9432
hyejin1231 <phj981231@naver.com>
2023-06-29 13:13:47 +0900

    20230629 토비의 스프링 부트 이해와 원리 조건부 자동 구성 #5.자동 구성 정보 대체하기

--- src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -19,6 +20,7 @@ import tobyspring.config.MyAutoConfiguration;
 @MyAutoConfiguration
 public class JettyWebServerConfig
 {
+	@ConditionalOnMissingBean
 	@Bean("jettyWebServerFactory")
 	public ServletWebServerFactory servletWebServerFactory()
 	{
--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +16,12 @@ import tobyspring.config.MyAutoConfiguration;
 
 //@Configuration
 //@Conditional(TomcatWebServerConfig.TomcatCondition.class)
-@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat") // 우선 Tomcat 라이브러리가 있는지 1차로 확인해보고..
 @MyAutoConfiguration
 public class TomcatWebServerConfig
 {
 	@Bean("tomcatWebServerFactory")
+	@ConditionalOnMissingBean // 그 다음 사용자가 관련 빈을 만든게 있는지 확인해보고 만약 없다면 Bean으로 생성해달라... !
 	public ServletWebServerFactory servletWebServerFactory()
 	{
 		return new TomcatServletWebServerFactory();
