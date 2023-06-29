4e5014e2be00253d8ab739ad29d14b7a09f5d229
hyejin1231 <phj981231@naver.com>
2023-06-29 10:47:33 +0900

    20230629 토비의 스프링 부트 이해와 원리 조건부 자동 구성 #2.@Conditional과 Condition

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -3,11 +3,16 @@ package tobyspring.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 import tobyspring.config.MyAutoConfiguration;
 
 //@Configuration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 @MyAutoConfiguration
 public class TomcatWebServerConfig
 {
@@ -15,5 +20,14 @@ public class TomcatWebServerConfig
 	public ServletWebServerFactory servletWebServerFactory()
 	{
 		return new TomcatServletWebServerFactory();
+	}
+	
+	static class TomcatCondition implements Condition
+	{
+		@Override
+		public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata)
+		{
+			return false;
+		}
 	}
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,8 +8,8 @@ import tobyspring.config.MySpringBootApplication;
 public class HellobootApplication {
 	
 	public static void main(String[] args) {
-//		MySpringApplication.run(HellobootApplication.class, args);
-		SpringApplication.run(HellobootApplication.class, args);
+		MySpringApplication.run(HellobootApplication.class, args);
+//		SpringApplication.run(HellobootApplication.class, args);
 	}
 	
 	
