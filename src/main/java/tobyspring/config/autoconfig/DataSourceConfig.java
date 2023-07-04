e1d37c36754d832495e32a0ef6bec5294df26e23
hyejin1231 <phj981231@naver.com>
2023-07-04 10:33:04 +0900

    20230704 토비의 스프링 부트 이해와 원리 스프링 JDBC 자동 구성 개발 #2. DataSource 자동 구성 클래스

---
+++ src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -0,0 +1,50 @@
+package tobyspring.config.autoconfig;
+
+import java.sql.Driver;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import tobyspring.config.ConditionalMyOnClass;
+import tobyspring.config.EnableMyConfigurationProperties;
+import tobyspring.config.MyAutoConfiguration;
+
+@MyAutoConfiguration
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
+@EnableMyConfigurationProperties(MyDataSourceProperties.class)
+public class DataSourceConfig
+{
+	@Bean
+	@ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource") // 1. HikariDataSource가 있는 경우에 빈이 만들어지도록
+	DataSource hikariDataSource(MyDataSourceProperties properties)
+	{
+		HikariDataSource dataSource = new HikariDataSource();
+		
+		dataSource.setDriverClassName(properties.getDriverClassName());
+		dataSource.setJdbcUrl(properties.getUrl());
+		dataSource.setUsername(properties.getUsername());
+		dataSource.setPassword(properties.getPassword());
+		
+		return dataSource;
+	}
+	@Bean
+	@ConditionalOnMissingBean // 2. HikariDataSource가 없으면 이 빈이 만들어지도록 설정
+	DataSource dataSource(MyDataSourceProperties properties)
+			throws ClassNotFoundException
+	{
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
+		dataSource.setUrl(properties.getUrl());
+		dataSource.setUsername(properties.getUsername());
+		dataSource.setPassword(properties.getPassword());
+		
+		return dataSource;
+	}
+	
+}
