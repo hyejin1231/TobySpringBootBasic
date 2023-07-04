bddd811cdec18417c3090b9cfa647ba50ae0ab75
hyejin1231 <phj981231@naver.com>
2023-07-04 11:14:49 +0900

    20230704 토비의 스프링 부트 이해와 원리 스프링 JDBC 자동 구성 개발 #4. HelloRepository

---
+++ src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -0,0 +1,38 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HelloBootTest
+public class HelloRepositoryTest
+{
+	@Autowired HelloRepository helloRepository;
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	@BeforeEach
+	void init()
+	{
+		jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS HELLO(NAME VARCHAR(50) PRIMARY KEY, COUNT INT)");
+	}
+	@Test
+	void findHelloFailed()
+	{
+		Assertions.assertThat(helloRepository.findHello("HYEJIN")).isNull();
+	}
+	
+	@Test
+	void increaseCount()
+	{
+		Assertions.assertThat(helloRepository.countOf("HYEJIN")).isEqualTo(0);
+		
+		helloRepository.increaseCount("HYEJIN");
+		Assertions.assertThat(helloRepository.countOf("HYEJIN")).isEqualTo(1);
+		
+		helloRepository.increaseCount("HYEJIN");
+		Assertions.assertThat(helloRepository.countOf("HYEJIN")).isEqualTo(2);
+	}
+}
